cd67a886662ee1fba315807c88099ae3f91da9f9
Mike A. Trethewey <miketrethewey@gmail.com>
2021-01-17 15:21:42 -0800

    Update to be usable

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,11 +1,19 @@
+-- Items
 Tracker:AddItems("items/inventory.json") -- All Inventory items
 
+-- Grids
 Tracker:AddLayouts("layouts/grids/powerups.json") -- Powerups grid
 Tracker:AddLayouts("layouts/grids/upgrades.json") -- Upgrades grid
 Tracker:AddLayouts("layouts/grids/weapons.json")  -- Weapons grid
 
 Tracker:AddLayouts("layouts/grids/grids.json") -- Combined grid
 
--- Items-Only
-Tracker:AddLayouts("layouts/items_only/tracker.json") -- Main Tracker
-Tracker:AddLayouts("layotus/items-only/broadcast.json") -- Broadcast View
+-- Locations
+Tracker:AddLocations("locations/locations.json")
+
+-- Maps
+Tracker:AddMaps("maps/maps.json")
+
+-- Default/Items-Only
+Tracker:AddLayouts("layouts/tracker.json") -- Main Tracker
+Tracker:AddLayouts("layouts/broadcast.json") -- Broadcast View

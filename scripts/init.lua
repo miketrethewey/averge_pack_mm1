99beab0a6e2827f07e5f98d3f5b16952ea75699b
Mike A. Trethewey <miketrethewey@gmail.com>
2021-01-17 14:37:26 -0800

    Add more body

---
+++ scripts/init.lua
@@ -0,0 +1,11 @@
+Tracker:AddItems("items/inventory.json") -- All Inventory items
+
+Tracker:AddLayouts("layouts/grids/powerups.json") -- Powerups grid
+Tracker:AddLayouts("layouts/grids/upgrades.json") -- Upgrades grid
+Tracker:AddLayouts("layouts/grids/weapons.json")  -- Weapons grid
+
+Tracker:AddLayouts("layouts/grids/grids.json") -- Combined grid
+
+-- Items-Only
+Tracker:AddLayouts("layouts/items_only/tracker.json") -- Main Tracker
+Tracker:AddLayouts("layotus/items-only/broadcast.json") -- Broadcast View

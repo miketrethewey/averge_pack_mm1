0f39ec916e9c505aeb316765a34cb6f4d70b3801
Mike A. Trethewey <miketrethewey@gmail.com>
2021-01-28 23:39:57 -0800

    Fix Inventory helpers

--- scripts/inventory.lua
+++ scripts/inventory.lua
@@ -1,38 +1,25 @@
+function hasAmount(code)
+  local num = Tracker:ProviderCountForCode(code)
+  return num
+end
+
 function has(code, level)
   if level == nil then
     level = 1
   end
-
-  local num = Tracker:ProviderCountForCode(code)
-  if num >= level then
-    return 1
-  else
-    return 0
-  end
+  return hasAmount(code) >= level
 end
 
 function canUseRemoteDrone()
-  if has("remotedrone") then
-    return 1
-  else
-    return 0
-  end
+  return has("remotedrone")
 end
 
 function canDroneTeleport()
-  if canUseRemoteDrone() and has("droneteleport") then
-    return 1
-  else
-    return 0
-  end
+  return canUseRemoteDrone() and has("droneteleport")
 end
 
 function canEnhancedDroneLaunch()
-  if canUseRemoteDrone() and has("enhanceddronelaunch") then
-    return 1
-  else
-    return 0
-  end
+  return canUseRemoteDrone() and has("enhanceddronelaunch")
 end
 
 function canShootLong()

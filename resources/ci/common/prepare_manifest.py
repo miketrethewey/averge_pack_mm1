a40aad4afe9d028ca75fada0444be6b79968331d
Mike A. Trethewey <miketrethewey@gmail.com>
2022-01-17 14:06:33 -0800

    Try to update version number dynamically

---
+++ resources/ci/common/prepare_manifest.py
@@ -0,0 +1,28 @@
+import common
+import json
+import os                 # for env vars
+from shutil import copy   # file manipulation
+
+def prepare_manifest():
+  env = common.prepare_env()
+
+  APPVERSION = ""
+
+  # set tag to app_version.txt
+  with open(os.path.join("..", "build", "app_version.txt"), "r") as f:
+      APPVERSION = f.readlines()[0]
+
+  if APPVERSION != "":
+      with open(os.path.join("manifest.json"), "r+") as manifestFile:
+          manifestJSON = json.load(manifestFile)
+          manifestJSON["package_version"] = APPVERSION
+          manifestFile.truncate()
+          manifestFile.write(json.dumps(manifestJSON))
+
+def main():
+  prepare_manifest()
+
+if __name__ == "__main__":
+  main()
+else:
+  raise AssertionError("Script improperly used as import!")
